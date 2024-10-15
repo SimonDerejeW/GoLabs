d9a5c7cac31d2c5ca1349be3432a68337b483045
SimonDerejeW <simondereje1@gmail.com>
2024-10-15 21:34:12 +0300

    activity 2 (Variables)

---
+++ lab1/activity-2/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main (){
+	var a int = 4;
+	var b float64 = 4.32;
+	var c string = "Golang";
+	var d bool = true;
+	e:="Short Declaration";
+
+	//Printing
+	fmt.Println("Integer: ", a);
+	fmt.Println("Float: ", b);
+	fmt.Println("String: ", c);
+	fmt.Println("Boolean: ",d);
+	fmt.Println("Short Declaration Format: ", e)
+}
