1d087d9afc659355afb303f2ebc91f83e0324596
SimonDerejeW <simondereje1@gmail.com>
2024-10-16 15:39:26 +0300

    activity-7 (ErrorHandling)

---
+++ lab1/activity-7/error_handling.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func readFile(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil{
+		return err
+	}
+	defer file.Close()
+
+	fmt.Println("File Opened Successfully: ", filename)
+	return nil
+}
+
+func main() {
+	err:=readFile("test.txt")
+	if err != nil{
+		fmt.Println("Error: ",err)
+	} else{
+		fmt.Println("File Successfully Read")
+	}
+}
