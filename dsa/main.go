c5225673e8e1831736da9f12ed5aca0df59cb607
SimonDerejeW <simondereje1@gmail.com>
2024-10-24 16:11:52 +0300

    added palindrome checker

--- dsa/main.go
+++ dsa/main.go
@@ -15,14 +15,38 @@ func freqCount(word string) map[string]int {
 	return counter
 }
 
+func isPalindrome (word string) bool {
+	i:=0
+	j:=(len(word) - 1)
 
+	for i <= j {
+		if (word[i] == word[j]){
+			i += 1
+			j -= 1
+		} else{
+			return false
+		}
+	}
+	return true
+
+
+}
 
 func main() {
-	fmt.Println("Enter word: ")
+	fmt.Println("Enter word to count its character's count: ")
 	word := utils.ReadStringInput()
 
 	word1 := utils.RemoveWhiteSpace(word)
 	word2 := utils.ToLowerCase(word1)
 
 	fmt.Println(freqCount(word2))
+
+	// Palindrome Checker
+	fmt.Println("Enter a word to check if palindrone: ")
+
+	palindrome := utils.ReadStringInput()
+	palindrome1 := utils.RemoveWhiteSpace(palindrome)
+	palindrome2 := utils.ToLowerCase(palindrome1)
+	
+	fmt.Println(isPalindrome(palindrome2))
 }
